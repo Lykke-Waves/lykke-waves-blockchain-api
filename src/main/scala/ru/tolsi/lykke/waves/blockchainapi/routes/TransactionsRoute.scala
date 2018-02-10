a93f6f1a2edc550ea03413f9adb50dd299188366
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-02-10 16:12:55 +0300

    realized transactions route

--- src/main/scala/ru/tolsi/lykke/waves/blockchainapi/routes/TransactionsRoute.scala
+++ src/main/scala/ru/tolsi/lykke/waves/blockchainapi/routes/TransactionsRoute.scala
@@ -1,6 +1,10 @@
 package ru.tolsi.lykke.waves.blockchainapi.routes
 
-import ru.tolsi.lykke.waves.blockchainapi.repository.BroadcastOperationsStore
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+import play.api.libs.json.JsBoolean
+import ru.tolsi.lykke.waves.blockchainapi.repository.{BroadcastOperation, BroadcastOperationsStore}
 
 
 //  [POST] /api/transactions/broadcast
@@ -12,6 +16,18 @@ import ru.tolsi.lykke.waves.blockchainapi.repository.BroadcastOperationsStore
 // X [GET] /api/transactions/broadcast/single/{operationId}
 // X [GET] /api/transactions/broadcast/many-inputs/{operationId}
 // X [GET] /api/transactions/broadcast/many-outputs/{operationId}
-case class TransactionsRoute(store: BroadcastOperationsStore) {
-
+case class TransactionsRoute(store: BroadcastOperationsStore) extends PlayJsonSupport {
+  val route: Route = path("transactions" / "broadcast") {
+    pathEnd {
+      post {
+        entity(as[BroadcastOperation]) { broadcastOperation =>
+          complete(store.addBroadcastOperation(broadcastOperation).map(JsBoolean))
+        }
+      }
+    } ~ path(Segment) { operationId =>
+      delete {
+        complete(store.removeBroadcastOperation(operationId).map(JsBoolean))
+      }
+    }
+  }
 }
