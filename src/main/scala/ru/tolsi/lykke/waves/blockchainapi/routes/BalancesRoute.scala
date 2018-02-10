060eb2db2c87be3896d97eb16a7d7bd6051279b0
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-02-10 14:58:57 +0300

    realized balances route

--- src/main/scala/ru/tolsi/lykke/waves/blockchainapi/routes/BalancesRoute.scala
+++ src/main/scala/ru/tolsi/lykke/waves/blockchainapi/routes/BalancesRoute.scala
@@ -1,10 +1,28 @@
 package ru.tolsi.lykke.waves.blockchainapi.routes
 
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+import play.api.libs.json.{JsBoolean, Json}
 import ru.tolsi.lykke.waves.blockchainapi.repository.BalancesStore
 
 //  [POST] /api/balances/{address}/observation
 //  [DELETE] /api/balances/{address}/observation
 //  [GET] /api/balances?take=integer&[continuation=string]
-case class BalancesRoute(store: BalancesStore) {
-
+case class BalancesRoute(store: BalancesStore) extends PlayJsonSupport {
+  val route: Route = path("balances") {
+    pathEnd {
+      get {
+        parameters('take.as[Int], 'continuation.as[Option[String]]) { case (take, continuation) =>
+          complete(store.getBalances(take, continuation).map(Json.toJson))
+        }
+      }
+    } ~ path(Segment / "observation") { address =>
+      post {
+        complete(store.addObservation(address).map(JsBoolean))
+      } ~ delete {
+        complete(store.removeObservation(address).map(JsBoolean))
+      }
+    }
+  }
 }
